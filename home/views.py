cbfc56338b6af12a8dfdc9214040bd20e549d316
Harshit Gupta <harshitg31897@outlook.com>
2023-12-30 19:20:16 +0530

    'user-authorization'

--- home/views.py
+++ home/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+import datetime
+from django.contrib.auth.decorators import login_required
 
 def home(request):
-    return HttpResponse('Hello, World !')
+    # return HttpResponse('Hello, World !')
+    return render(request, 'home/welcome.html', {})
+
+
+@login_required(login_url='/admin')
+def authorized(request):
+    return render(request, 'home/authorized.html', {'currentTime': datetime.datetime.now()})
