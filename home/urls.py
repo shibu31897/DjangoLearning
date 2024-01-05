ef6bc2436eb314e789075b52f2c7e171cf250d87
Harshit Gupta <harshitg31897@outlook.com>
2024-01-05 21:27:17 +0530

    more implementation class based view

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('home', views.HomeView.as_view()),
-    path('authorized', views.AuthorizedView.as_view()),
-]
+# urlpatterns = [
+#     path('home', views.HomeView.as_view()),
+#     path('authorized', views.AuthorizedView.as_view()),
+# ]
--- notes/views.py
+++ notes/views.py
@@ -3,8 +3,20 @@ from django.shortcuts import render
 from .models import Notes
 
 from django.http import Http404
+from django.views.generic import ListView,DetailView
 
 # Create your views here.
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = 'notes/notes_list.html'
+
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+
+
 
 def list(request):
     all_notes = Notes.objects.all()
